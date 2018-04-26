f24e0bb3b5343fa825b191053f934be60a21ff93
MarcusPaiva <marcuspaiva2014@gmail.com>
2018-04-25 22:11:10 -0300

    Update plugin.js
    Cleaning in project code

--- www/plugin.js
+++ www/plugin.js
@@ -4,12 +4,6 @@ var exec = require('cordova/exec');
 var PLUGIN_NAME = 'Toast';
 
 var MyCordovaPlugin = {
-//  echo: function(phrase, cb) {
-//    exec(cb, null, PLUGIN_NAME, 'echo', [phrase]);
-//  },
-//  getDate: function(cb) {
-//    exec(cb, null, PLUGIN_NAME, 'getDate', []);
-//  },
   toast:function(phrase) {
     exec(null, null, PLUGIN_NAME, 'toast', [phrase]);
   }
